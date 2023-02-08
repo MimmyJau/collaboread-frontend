1ca745ab357727975a96947995e86041fc3edad9
Jimmy <jimmy.b.mau@gmail.com>
2023-02-08 16:31:56 -0500

    Add tiptap module and components

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
+import Tiptap from '../components/Tiptap'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -15,7 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <p>Hi</p>
+        <Tiptap />
       </main>
     </>
   )
