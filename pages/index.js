ab29bb35be89f27b60deb36275fa957d019366e0
Jimmy <jimmy.b.mau@gmail.com>
2023-02-09 11:35:19 -0500

    Add tailwind and remove base styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 
 import Tiptap from '../components/Tiptap'
 
@@ -16,7 +15,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className={inter.className}>
+        <h1 className="text-3xl font-bold underline text-indigo-700">
+          Hello world!
+        </h1>
         <Tiptap />
       </main>
     </>
