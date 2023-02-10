820b4e263f0dffd5ab34bb063f9752301b5481be
Jimmy <jimmy.b.mau@gmail.com>
2023-02-09 22:19:43 -0500

    Add basic functionality to TipTap

--- components/Tiptap.js
+++ components/Tiptap.js
@@ -1,16 +1,58 @@
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import Highlight from '@tiptap/extension-highlight'
+
+const printJSON = (editor) => {
+  console.log(editor.getJSON())
+}
+
+const printHTML = (editor) => {
+  console.log(editor.getHTML())
+}
+
+const printText = (editor) => {
+  console.log(editor.getText())
+}
+
+const MenuToolbar = ({ editor }) => {
+  return (
+    <>
+      <button onClick={() => editor.chain().focus().toggleBold().run()}>
+        Bold
+      </button>
+      <button onClick={() => editor.chain().focus().toggleItalic().run()}>
+        Italics
+      </button>
+      <button onClick={() => editor.chain().focus().toggleHighlight().run()}>
+        Highlight
+      </button>
+      <button onClick={() => printJSON(editor)}>
+        JSON
+      </button>
+      <button onClick={() => printHTML(editor)}>
+        HTML
+      </button>
+      <button onClick={() => printText(editor)}>
+        Text
+      </button>
+    </>
+  )
+}
 
 const Tiptap = () => {
   const editor = useEditor({
     extensions: [
       StarterKit,
+      Highlight,
     ],
-    content: '<p>Hello World! 🌎</p>',
+    content: '<p>Hello World! 🌎</p><p></p><p>and this</p><p></p><p>but that</p>',
   })
 
   return (
-    <EditorContent editor={editor} />
+    <>
+      <MenuToolbar editor={editor}/>
+      <EditorContent editor={editor} />
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={inter.className}>
-        <h1 className="text-3xl font-bold underline text-indigo-700">
+        <h1 className="text-3xl font-bold underline text-green-700">
           Hello world!
         </h1>
         <Tiptap />
