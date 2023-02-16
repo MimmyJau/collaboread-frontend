e955d9997a494f943e927bbff0298a35e8ddc387
Jimmy <jimmy.b.mau@gmail.com>
2023-02-16 14:26:48 -0500

    Refactor to use dangerouslySetInnerHTML

--- components/Reader.js
+++ components/Reader.js
@@ -11,6 +11,21 @@ const ShowHighlightButton = (props) => {
   )
 }
 
+const innerHTML = `
+  <br />
+  <h2>Welcome to the reader</h2>
+  <p></p>
+  <p>This is where we run tests on selection and annotation.</p>
+  <br />
+  <ul className="list-disc pl-5">
+    <li>The first point being made</li>
+    <li>The second point is <i>less important</i> than the first.</li>
+  </ul>
+  <div>
+    <span>This <b>is </b><b>span 1.</b> This is span 2.</span>
+  </div>
+`
+
 const Reader = () => {
   const [ highlight, setHighlight ] = useState(null);
   const [ anchorOffset, setAnchorOffset ] = useState(0);
@@ -22,20 +37,7 @@ const Reader = () => {
   }
 
   return (
-    <div className="reader" onMouseUp={setSelectedText}>
-      <br />
-      <h2>Welcome to the reader</h2>
-      <p></p>
-        <p>This is where we run tests on selection and annotation.</p>
-        <br />
-        <ul className="list-disc pl-5">
-          <li>The first point being made</li>
-          <li>The second point is <i>less important</i> than the first.</li>
-        </ul>
-        <div>
-          <span>This <b>is </b><b>span 1.</b> This is span 2.</span>
-      </div>
-      <ShowHighlightButton highlight={highlight} anchorOffset={anchorOffset}/>
+    <div className="reader" onMouseUp={setSelectedText} dangerouslySetInnerHTML={{__html: innerHTML}}>
     </div>
   )
 }
