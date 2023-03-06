dc447ed967b42e55bda9dac4019cf368819bc8cd
Jimmy <jimmy.b.mau@gmail.com>
2023-03-05 22:24:55 -0500

    Add react-query wrapper around _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 import { polyfill } from "interweave-ssr";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "@/styles/globals.css";
 
@@ -7,5 +15,13 @@ import "@/styles/globals.css";
 polyfill();
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
