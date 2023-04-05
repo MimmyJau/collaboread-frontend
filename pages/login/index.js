664461dd52449cae5de1b33caf7e990c722a3bba
Jimmy <jimmy.b.mau@gmail.com>
2023-04-05 19:02:12 -0400

    Refactor folder structure to match nextjs

--- pages/login/index.js
+++ pages/login/index.js
@@ -11,8 +11,6 @@ const LoginForm = () => {
     postLogin.mutate(data);
   });
 
-  console.log(watch("username"));
-
   return (
     <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
