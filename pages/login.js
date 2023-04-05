5ffb7c7de3d99dd11062d9fe78010a99add74bd3
Jimmy <jimmy.b.mau@gmail.com>
2023-04-05 16:40:59 -0400

    WIP change color of form

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,17 @@
 import { useForm } from "react-hook-form";
+import { usePostLogin } from "hooks";
 
 const LoginForm = () => {
   const { register, handleSubmit, watch } = useForm();
+  const postLogin = usePostLogin();
 
   // Will only run if form passes validation.
   const onSubmit = handleSubmit((data) => {
     console.log(data);
+    postLogin.mutate(data);
   });
+
+  console.log(watch("username"));
 
   return (
     <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
@@ -20,18 +25,18 @@ const LoginForm = () => {
           <input type="hidden" name="remember" value="true" />
           <div className="-space-y-px rounded-md shadow-sm">
             <div>
-              <label htmlFor="username-or-email" className="sr-only">
+              <label htmlFor="username" className="sr-only">
                 Email address
               </label>
               <input
-                id="username-or-email"
-                name="username-or-email"
+                id="username"
+                name="username"
                 type="text"
                 autoComplete="email"
                 required
-                className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                 placeholder="Username or Email address"
-                {...register("username-or-email")}
+                {...register("username")}
               />
             </div>
             <div>
@@ -44,7 +49,7 @@ const LoginForm = () => {
                 type="password"
                 autoComplete="current-password"
                 required
-                className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                 placeholder="Password"
                 {...register("password")}
               />
@@ -52,10 +57,10 @@ const LoginForm = () => {
           </div>
 
           <div>
-            <button className="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            <button className="group relative flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
               <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                 <svg
-                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                  className="h-5 w-5 text-blue-500 group-hover:text-blue-400"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                   aria-hidden="true"
@@ -75,7 +80,7 @@ const LoginForm = () => {
             <div className="text-sm">
               <a
                 href="#"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
+                className="font-medium text-blue-600 hover:text-blue-500"
               >
                 Forgot your password?
               </a>
