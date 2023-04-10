762288a1a06aebe4732715f485f31e07a857b46a
Jimmy <jimmy.b.mau@gmail.com>
2023-04-10 11:30:24 -0400

    Add feed to home page with some basic designs

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,29 @@ import Link from "next/link";
 
 import NavBar from "components/NavBar";
 
-export default function Home() {
+const Feed = () => {
   return (
-    <>
-      <NavBar />
-      <div className="border border-green-400 flex justify-center content-center">
-        <div className="border border-blue-400 w-7/12">
-          <h1 className="text-4xl">Books</h1>
+    <div className="flex justify-center content-center py-2">
+      <div className="w-7/12">
+        <h1 className="font-semibold text-4xl my-5">Books (more to come!)</h1>
+        <div className="bg-yellow-50 rounded-lg hover:bg-yellow-100 hover:rounded-lg">
           <Link href="/a/3d7fbac8-214d-4fa4-a157-f60b603b8980">
-            <div className="border border-red-400 p-3">
+            <div className="p-5">
               <h2 className="text-xl">Thus Spoke Zarathustra</h2>
               <h3 className="text-sm">by Friedrich Nietzsche</h3>
             </div>
           </Link>
         </div>
       </div>
+    </div>
+  );
+};
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <Feed />
     </>
   );
 }
