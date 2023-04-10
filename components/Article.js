d61d7dacd3273fab64a3116f8d4e2ec34c7f9829
Jimmy <jimmy.b.mau@gmail.com>
2023-04-10 10:24:36 -0400

    Fix runtime error module ClassApplier not found
    There was a bug when routing from home page to [article_uuid] page where
    module ClassApplier was not found. Only happened when routing from
    another page, did not happen when loading the page directly.
    
    Seems to be fixed simply by re-ordering imports.

--- components/Article.js
+++ components/Article.js
@@ -2,9 +2,9 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { Interweave } from "interweave";
 import rangy from "rangy";
-import "rangy/lib/rangy-highlighter";
 import "rangy/lib/rangy-classapplier";
 import "rangy/lib/rangy-textrange";
+import "rangy/lib/rangy-highlighter";
 import { useCreateAnnotation, useDeleteAnnotation } from "hooks";
 
 function getHighlightableRoot() {
