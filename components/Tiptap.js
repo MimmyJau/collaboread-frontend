29951f5d9fce6eafc5b97e4906bf10913ca16bd0
Jimmy <jimmy.b.mau@gmail.com>
2023-05-01 15:42:06 -0400

    Fix styling of editor

--- components/Tiptap.js
+++ components/Tiptap.js
@@ -18,35 +18,56 @@ const printText = (editor) => {
   console.log(editor.getText());
 };
 
+const MenuButton = ({ name, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="px-2 py-1 mr-1 bg-gray-100 rounded hover:bg-gray-200 hover:text-blue-500"
+    >
+      {name}
+    </button>
+  );
+};
+
 const MenuToolbar = ({ editor }) => {
   return (
-    <>
-      <button onClick={() => editor.chain().focus().toggleBold().run()}>
-        Bold
-      </button>
-      <button onClick={() => editor.chain().focus().toggleItalic().run()}>
-        Italics
-      </button>
-      <button
+    <div className="sticky z-10 top-0 bg-white">
+      <MenuButton
+        name="Bold"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+      />
+      <MenuButton
+        name="Italics"
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+      />
+      <MenuButton
+        name="h1"
+        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+      />
+      <MenuButton
+        name="h2"
         onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-      >
-        Section (H2)
-      </button>
-      <button
+      />
+      <MenuButton
+        name="h3"
         onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-      >
-        Subsection (H3)
-      </button>
-      <button onClick={() => editor.chain().focus().toggleHighlight().run()}>
-        Highlight
-      </button>
-      <button onClick={() => editor.chain().focus().toggleBulletList().run()}>
-        toggleBulletList
-      </button>
-      <button onClick={() => printJson(editor)}>JSON</button>
-      <button onClick={() => printHtml(editor)}>HTML</button>
-      <button onClick={() => printText(editor)}>Text</button>
-    </>
+      />
+      <MenuButton
+        name="h4"
+        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+      />
+      <MenuButton
+        name="Highlight"
+        onClick={() => editor.chain().focus().toggleHighlight().run()}
+      />
+      <MenuButton
+        name="Bullets"
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+      />
+      <MenuButton name="JSON" onClick={() => printJson(editor)} />
+      <MenuButton name="HTML" onClick={() => printHtml(editor)} />
+      <MenuButton name="Text" onClick={() => printText(editor)} />
+    </div>
   );
 };
 
@@ -74,10 +95,17 @@ const Tiptap = (props) => {
     content:
       props.content ||
       "<p>Hi this is a test document for collaboread. It consists of <strong>bold text</strong>, <em>italics text</em>, and combined <strong><em>bold and italics</em></strong>. It also consists of the following bullet points:</p><ul><li><p>This is the first bullet point. It might have some <em>styled text</em>.</p></li><li><p>This is the second bullet point, it might not have any textx.</p></li><li><p>This is the third and last bullet point. It's important that we can highlight across bullet points :)</p></li></ul><p>This is the last paragraph. Hope you enjoyed!</p>",
+    editorProps: {
+      attributes: {
+        class:
+          props.className ||
+          "prose bg-white rounded border border-gray-200 overflow-y-scroll focus:outline-none focus:border focus:border-gray-400 hover:border hover:border-gray-300",
+      },
+    },
   });
 
   return (
-    <div className="prose">
+    <div className="">
       <MenuToolbar editor={editor} />
       <EditorContent editor={editor} />
     </div>
