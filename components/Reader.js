f4164c2a3345b42a3b32b9f72bf4be42b1e30e8f
Jimmy <jimmy.b.mau@gmail.com>
2023-06-05 15:55:22 -0400

    Fix style of Table of Contents

--- components/Reader.js
+++ components/Reader.js
@@ -74,7 +74,7 @@ const Reader = (props) => {
       className="grid grid-cols-6 gap-1 h-full overflow-hidden"
       onMouseUp={(e) => handleMouseUp(e)}
     >
-      <TableOfContents className="hidden md:grid col-start-1 col-span-1 overflow-y-auto px-3 pb-10" />
+      <TableOfContents className="hidden md:flex md:flex-col col-start-1 col-span-1 overflow-y-auto px-3 pb-10" />
       <Article
         className="col-start-1 col-span-6 md:col-start-2 md:col-span-3 md:place-self-end px-2 overflow-y-auto h-full w-full"
         html={article.articleHtml}
