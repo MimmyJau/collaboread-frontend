cc6f001d199f0bc01faa705e6138a1b57849702e
Jimmy <jimmy.b.mau@gmail.com>
2023-07-07 16:17:52 -0400

    Fix edit no longer working with new hook

---
+++ pages/e/[...slug].js
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import { Editor } from "components/ArticleEditor";
+import NavBar from "components/NavBar";
+import { useFetchArticle } from "hooks";
+import { useGetUrl } from "hooks/pages";
+
+export default function Page() {
+  const { path } = useGetUrl();
+  const { data: article, status } = useFetchArticle(path);
+
+  if (status !== "success") return null;
+  console.log(article.articleHtml);
+  return (
+    <>
+      <NavBar />
+      <Editor content={article.articleHtml} />
+    </>
+  );
+}
